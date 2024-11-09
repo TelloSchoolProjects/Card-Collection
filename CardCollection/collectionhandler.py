75be8ce114c0fb9e7758b8e39ec7c930ca391822
TelloViz <lollisjosh@csu.fullerton.edu>
2024-11-08 20:07:03 -0800

    create file for class collectionhandler

---
+++ CardCollection/collectionhandler.py
@@ -0,0 +1,12 @@
+# This Python file uses the following encoding: utf-8
+
+
+class CollectionHandler:
+    def __init__(self):
+        pass
+
+    def handle_load_collection():
+        pass
+
+    def handle_save_collectoin():
+        pass
